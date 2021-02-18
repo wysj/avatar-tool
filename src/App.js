bb93c52bb7a2b2d5084bde6dd91aaf74725b887a
MYSOFT\zhangym03 <zhangym03@mingyuanyun.com>
2021-02-18 17:30:42 +0800

    feat:  头像工具开发

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
-import { Row, Col, Input, Button, Popover } from 'antd';
+import { Row, Col, Input, Button, Popover, Layout } from 'antd';
 import { SketchPicker } from 'react-color';
 
 import './App.css';
+
+const { Header, Content } = Layout;
 
 const WIDTH = 300;
 const HEIGHT = 300;
@@ -52,56 +54,64 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Row>
-        <Col span="12">
+    <Layout>
+      <Header>
+        <span className="header">
+          头像工具
+        </span>
+      </Header>
+      <Content className="App">
+        <div className="app-body">
           <Row>
-            <Col span={6}>内容：</Col>
-            <Col span={12}>
-              <Input maxLength="2" value={text} onInput={(e) => { setText(e.target.value) }} />
+            <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+              <Row>
+                <Col span={6}>内容：</Col>
+                <Col span={12}>
+                  <Input maxLength="2" value={text} onInput={(e) => { setText(e.target.value) }} />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>文字颜色：</Col>
+                <Col span={12}>
+                  <Popover
+                    style={{ padding: 0 }}
+                    content={
+                      <SketchPicker
+                        color={color}
+                        onChangeComplete={(value) => setColor(value.hex)}
+                      />
+                    }>
+                    <Input maxLength="2" value={color} onInput={(e) => { setColor(e.target.value) }} />
+                  </Popover>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>背景颜色：</Col>
+                <Col span={12}>
+                  <Popover
+                    style={{ padding: 0 }}
+                    content={
+                      <SketchPicker
+                        color={background}
+                        onChangeComplete={(value) => setBackground(value.hex)}
+                      />
+                    }>
+                    <Input maxLength="2" value={background} onInput={(e) => { setBackground(e.target.value) }} />
+                  </Popover>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}></Col>
+                <Col span={12}><Button type="primary" onClick={() => { handleDownload() }}>下载图片</Button></Col>
+              </Row>
+            </Col>
+            <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+              <canvas id="avatar" width="300" height="300" />
             </Col>
           </Row>
-          <Row>
-            <Col span={6}>文字颜色：</Col>
-            <Col span={12}>
-              <Popover
-                style={{ padding: 0 }}
-                content={
-                  <SketchPicker
-                    color={color}
-                    onChangeComplete={(value) => setColor(value.hex)}
-                  />
-                }>
-                <Input maxLength="2" value={color} onInput={(e) => { setColor(e.target.value) }} />
-              </Popover>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={6}>背景颜色：</Col>
-            <Col span={12}>
-              <Popover
-                style={{ padding: 0 }}
-                content={
-                  <SketchPicker
-                    color={background}
-                    onChangeComplete={(value) => setBackground(value.hex)}
-                  />
-                }>
-                <Input maxLength="2" value={background} onInput={(e) => { setBackground(e.target.value) }} />
-              </Popover>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={6}></Col>
-            <Col span={12}><Button type="primary" onClick={() => { handleDownload() }}>下载图片</Button></Col>
-          </Row>
-
-        </Col>
-        <Col span="12">
-          <canvas id="avatar" width="300" height="300" />
-        </Col>
-      </Row>
-    </div>
+        </div>
+      </Content>
+    </Layout>
   );
 }
 
